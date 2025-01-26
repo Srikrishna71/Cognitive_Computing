deae2908e168f75992f22f533b903342eade9489
Srikrishna71 <srikrishnavadlamani@gmail.com>
2025-01-26 23:20:51 +0530

    Create 1.py

---
+++ assignment1/1.py
@@ -0,0 +1,37 @@
+# 1.1
+print('krishna')
+print('krishna')
+print('krishna')
+
+# 2.1
+a = 1
+b = 2
+c = 3
+d = a+b+c
+print(d)
+
+# 2.2
+a = "st"
+b = "ri"
+c = "ng"
+d = a+b+c
+print(d)
+
+# 4.1
+for i in range(1,11):
+  print(7,"*",i,"=",i*7)
+for i in range(1,11):
+  print(9,"*",i,"=",i*9)
+
+# 4.2
+n = int(input("Enter No: "))
+for i in range(1,11):
+  print(n,"*",i,"=",i*n)
+
+# 4.3
+n = int(input("Enter No: "))
+s = 0
+for i in range(1,n+1):
+  s=s+i
+print("sum is",s)
+
